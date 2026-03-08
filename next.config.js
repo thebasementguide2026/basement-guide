804f61f961d23fb9b4979f8afb3068aedc42b82c
thebasementguide2026 <pnason4@gmail.com>
2026-03-08 07:45:54 -0400

    Add 16 permanent 301 redirects for Bing Webmaster 404 fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,90 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/articles/common-renovation-mistakes',
+        destination: '/articles/basement-renovation-mistakes',
+        permanent: true,
+      },
+      {
+        source: '/get-quotes',
+        destination: '/get-quotes',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-waterproofing-costs',
+        destination: '/articles/basement-waterproofing-cost',
+        permanent: true,
+      },
+      {
+        source: '/articles/mold-prevention',
+        destination: '/articles/how-to-prevent-basement-mold',
+        permanent: true,
+      },
+      {
+        source: '/articles/sump-pump-guide',
+        destination: '/articles/sump-pump-buying-guide',
+        permanent: true,
+      },
+      {
+        source: '/articles/efflorescence-explainer',
+        destination: '/articles/basement-efflorescence',
+        permanent: true,
+      },
+      {
+        source: '/articles/finishing-a-basement',
+        destination: '/articles/basement-finishing-cost',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-mold-prevention',
+        destination: '/articles/how-to-prevent-basement-mold',
+        permanent: true,
+      },
+      {
+        source: '/articles/sump-pump-installation-guide',
+        destination: '/articles/sump-pump-cost',
+        permanent: true,
+      },
+      {
+        source: '/articles/how-to-hire-a-basement-contractor',
+        destination: '/articles/how-to-hire-basement-waterproofing-contractor',
+        permanent: true,
+      },
+      {
+        source: '/articles/sump-pump-installation-cost',
+        destination: '/articles/sump-pump-cost',
+        permanent: true,
+      },
+      {
+        source: '/articles/why-basement-floods-without-rain',
+        destination: '/articles/why-basement-floods-with-sump-pump',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-humidity',
+        destination: '/articles/basement-humidity-guide',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-waterproofing-methods',
+        destination: '/articles/complete-basement-waterproofing-guide',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-waterproofing-guide',
+        destination: '/articles/complete-basement-waterproofing-guide',
+        permanent: true,
+      },
+      {
+        source: '/articles/basement-waterproofing-cost-guide',
+        destination: '/articles/basement-waterproofing-cost',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
